566ed90b52dff3cd746b5c0101b6cf05c0fe7b4e
4jamesccraven <4jamesccraven@gmail.com>
2023-12-04 01:36:18 -0500

    Code organization

--- .shared_inc/card.h
+++ .shared_inc/card.h
@@ -6,11 +6,14 @@
     #include<vector>
     #include<random>
     #include<algorithm>
+    #include<ctime>
 
     namespace cards{
+        //card suits
         enum spanish_suit_type {Coins = 1, Swords = 2, Cups = 3, Rods = 4};
         enum french_suit_type {Hearts = 1, Diamonds = 2, Spades = 3, Clubs = 4};
 
+        //card
         template<typename T>
         struct card {
             T suit;
@@ -20,13 +23,17 @@
         template<typename T>
         std::ostream& operator<<(std::ostream& os, card<T> c);
 
+        //deck
         template<typename T>
         class deck {
         public:
-            deck();
-            void shuffle();
-            void print(std::ostream& os);
+            deck();                             //constructor
+
+            void shuffle();                     //deck manipulation
             card<T> deal();
+
+            void print(std::ostream& os);       //print methods
+            void printUsed(std::ostream& os);
         protected:
             std::vector<card<T>> internal_deck_;
             std::vector<card<T>> used_;
@@ -34,11 +41,12 @@
         };
 
 
+        //***---DECK---***//
+        //constructors
         template<>
         inline cards::deck<cards::spanish_suit_type>::deck() {
             size_ = 40;
             internal_deck_.resize(size_);
-            used_.resize(size_);
             unsigned int index = 0;
             std::for_each(internal_deck_.begin(), internal_deck_.end(), [&index](card<spanish_suit_type>& c){
                 unsigned int rank = (index % 10) + 1;
@@ -51,33 +59,10 @@
             });
         }
 
-        template <typename T>
-        inline void deck<T>::shuffle() {
-            std::random_device r;
-            std::mt19937 gen(r());
-            std::shuffle(internal_deck_.begin(), internal_deck_.end(), gen);
-        }
-
-        template <typename T>
-        inline void deck<T>::print(std::ostream &os) {
-            for (auto card : internal_deck_) {
-                os << card << std::endl;
-            }
-        }
-
-        template <typename T>
-        inline card<T> deck<T>::deal() {
-            card<T> dealt = internal_deck_.back();
-            internal_deck_.pop_back();
-            used_.push_back(dealt);
-            return dealt;
-        }
-
         template<>
         inline cards::deck<cards::french_suit_type>::deck() {
             size_ = 52;
             internal_deck_.resize(size_);
-            used_.resize(size_);
             unsigned int index = 0;
             std::for_each(internal_deck_.begin(), internal_deck_.end(), [&index](card<french_suit_type>& c){
                 unsigned int rank = (index % 13) + 1;
@@ -89,6 +74,39 @@
             });
         }
 
+        //deck manipulations
+        template <typename T>
+        inline void deck<T>::shuffle() {
+            std::mt19937 gen(std::time(NULL));
+            std::shuffle(internal_deck_.begin(), internal_deck_.end(), gen);
+        }
+
+        template <typename T>
+        inline card<T> deck<T>::deal() {
+            card<T> dealt = internal_deck_.back();
+            internal_deck_.pop_back();
+            used_.push_back(dealt);
+            return dealt;
+        }
+
+        //print methods
+        template <typename T>
+        inline void deck<T>::print(std::ostream &os) {
+            for (auto card : internal_deck_) {
+                os << card << std::endl;
+            }
+        }
+
+
+        template <typename T>
+        inline void deck<T>::printUsed(std::ostream &os) {
+            for (auto card : used_) {
+                os << card << std::endl;
+            }
+        }
+
+        //***---CARD---***//
+        //card printing overload
         template <typename T>
         std::ostream &operator<<(std::ostream &os, card<T> c) {
             os << c.rank << " of " << c.suit;
